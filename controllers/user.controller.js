0065931d3972c383a4eef7ebecf7e98534390442
Hyeonjin <oboki2001@naver.com>
2025-08-02 16:46:03 +0900

    [feat] 이메일/비밀번호 로그인 기능 및 userId 기반 사용자 조회 기능 추가

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -9,7 +9,7 @@ userController.createUser = async (req, res) => {
     if (user) {
       throw new Error("이미 가입된 유저입니다.");
     }
-    const salt = await bcrypt.genSaltSync(10);
+    const salt = await bcrypt.genSalt(10);
     password = await bcrypt.hash(password, salt);
     const newUser = new User({
       email,
@@ -19,6 +19,37 @@ userController.createUser = async (req, res) => {
     });
     await newUser.save();
     return res.status(200).json({ status: "success" });
+  } catch (error) {
+    res.status(400).json({ status: "failed", message: error.message });
+  }
+};
+
+userController.loginWithEmail = async (req, res) => {
+  try {
+    let { email, password } = req.body;
+    const user = await User.findOne({ email }, "-createdAt -updatedAt -__v");
+    if (!user) {
+      throw new Error("이메일이 존재하지 않습니다.");
+    }
+    const isPasswordSame = await bcrypt.compare(password, user.password);
+    if (isPasswordSame) {
+      const token = user.generateToken();
+      return res.status(200).json({ status: "success", user, token });
+    }
+    throw new Error("비밀번호가 일치하지 않습니다.");
+  } catch (error) {
+    res.status(400).json({ status: "failed", message: error.message });
+  }
+};
+
+userController.getUser = async (req, res) => {
+  try {
+    const { userId } = req;
+    const user = await User.findById(userId);
+    if (user) {
+      res.status(200).json({ status: "success", user });
+    }
+    throw new Error("Invalid Token");
   } catch (error) {
     res.status(400).json({ status: "failed", message: error.message });
   }
