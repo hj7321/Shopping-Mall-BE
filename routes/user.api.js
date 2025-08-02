e73a86ad3e82e40157310ab3ea63cdd8be1d7136
Hyeonjin <oboki2001@naver.com>
2025-08-02 16:50:06 +0900

    [feat] /login 및 /me API 라우터 구현 및 auth 미들웨어 연동

--- routes/user.api.js
+++ routes/user.api.js
@@ -1,8 +1,10 @@
 const express = require("express");
 const userController = require("../controllers/user.controller");
+const authCountroller = require("../controllers/auth.controller");
 const router = express.Router();
 
-// 회원가입
-router.post("/", userController.createUser);
+router.post("/", userController.createUser); // 회원가입
+router.post("/login", userController.loginWithEmail); // 로그인
+router.get("/me", authCountroller.authenticate, userController.getUser); // 토큰이 valid한 토큰인지, 이 토큰값을 가지고 유저를 찾아서 리턴
 
 module.exports = router;
