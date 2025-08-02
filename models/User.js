5b858a547c5b196f292ae4cbaa6d872130d4f666
Hyeonjin <oboki2001@naver.com>
2025-08-02 16:48:56 +0900

    [feat] User 스키마에 이메일/비밀번호 유효성 조건 추가 및 generateToken 메서드 구현

--- models/User.js
+++ models/User.js
@@ -1,15 +1,23 @@
+require("dotenv").config();
+const JWT_SECRET_KEY = process.env.JWT_SECRET_KEY;
 const mongoose = require("mongoose");
 const Schema = mongoose.Schema;
+const jwt = require("jsonwebtoken");
+
 const userSchema = Schema(
   {
     email: {
       type: String,
       required: true,
       unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^[^\s@]+@[^\s@]+\.[^\s@]+$/, "올바른 이메일 형식이 아닙니다."],
     },
     password: {
       type: String,
       required: true,
+      minlength: 8,
     },
     name: {
       type: String,
@@ -30,6 +38,13 @@ userSchema.methods.toJSON = function () {
   delete obj.updateAt;
   delete obj.createAt;
   return obj;
+};
+
+userSchema.methods.generateToken = function () {
+  const token = jwt.sign({ _id: this._id }, JWT_SECRET_KEY, {
+    expiresIn: "1d",
+  });
+  return token;
 };
 
 const User = mongoose.model("User", userSchema);
