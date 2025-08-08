79ea8e64ed0d4ff927ad6bd9ea44983a57daab96
Hyeonjin <oboki2001@naver.com>
2025-08-08 11:57:47 +0900

    [feat] 장바구니 API 라우트 구현

---
+++ routes/cart.api.js
@@ -0,0 +1,12 @@
+const express = require("express");
+const cartController = require("../controllers/cart.controller");
+const authCountroller = require("../controllers/auth.controller");
+const router = express.Router();
+
+router.post("/", authCountroller.authenticate, cartController.addItemToCart);
+router.get("/", authCountroller.authenticate, cartController.getCart);
+router.get("/qty", authCountroller.authenticate, cartController.getCartQty);
+router.put("/", authCountroller.authenticate, cartController.updateCart);
+router.delete("/", authCountroller.authenticate, cartController.deleteCart);
+
+module.exports = router;
