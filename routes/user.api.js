581754647187efcd0817fe4dccd1296986b02bfd
Hyeonjin <oboki2001@naver.com>
2025-08-11 01:09:54 +0900

    [feat] Google 로그인 라우트 추가

--- routes/user.api.js
+++ routes/user.api.js
@@ -5,6 +5,7 @@ const router = express.Router();
 
 router.post("/", userController.createUser); // 회원가입
 router.post("/login", userController.loginWithEmail); // 로그인
+router.post("/google", userController.loginWithGoogle); // 구글 로그인
 router.get("/me", authCountroller.authenticate, userController.getUser); // 토큰이 valid한 토큰인지, 이 토큰값을 가지고 유저를 찾아서 리턴
 
 module.exports = router;
