478e41f9010eae9b753700986808bf73f4d33f39
Hyeonjin <oboki2001@naver.com>
2025-08-11 00:54:13 +0900

    [feat] Google OAuth 로그인 기능 구현 및 환경변수 설정 추가

--- controllers/user.controller.js
+++ controllers/user.controller.js
@@ -1,6 +1,9 @@
 const User = require("../models/User");
 const bcrypt = require("bcryptjs");
+const { OAuth2Client } = require("google-auth-library");
 const userController = {};
+require("dotenv").config();
+const GOOGLE_CLIENT_ID = process.env.GOOGLE_CLIENT_ID;
 
 userController.createUser = async (req, res) => {
   try {
@@ -40,6 +43,38 @@ userController.loginWithEmail = async (req, res) => {
     return res
       .status(200)
       .json({ status: "success", user: user.toJSON(), token });
+  } catch (error) {
+    res.status(400).json({ status: "failed", message: error.message });
+  }
+};
+
+userController.loginWithGoogle = async (req, res) => {
+  // 백엔드에서 로그인하기
+  // 토큰값을 읽어와서 유저 정보를 뽑아내고 email
+  // (1) 이미 로그인을 한 적이 있는 유저 => 로그인 시키고, 토큰값 주면 됨!
+  // (2) 처음 로그인 시도를 한 유저 => 유저 정보 먼저 새로 생성하고, 토큰값 주면 됨!
+  try {
+    const { token } = req.body;
+    const googleClient = new OAuth2Client(GOOGLE_CLIENT_ID);
+    const ticket = await googleClient.verifyIdToken({
+      idToken: token,
+      audience: GOOGLE_CLIENT_ID,
+    });
+    const { email, name } = ticket.getPayload();
+    let user = await User.findOne({ email });
+    if (!user) {
+      const randomPassword = "" + Math.floor(Math.random() * 10000000);
+      const salt = await bcrypt.genSalt(10);
+      const newPassword = await bcrypt.hash(randomPassword, salt);
+      user = new User({
+        name,
+        email,
+        password: newPassword,
+      });
+      await user.save();
+    }
+    const sessionToken = await user.generateToken();
+    res.status(200).json({ status: "success", user, token: sessionToken });
   } catch (error) {
     res.status(400).json({ status: "failed", message: error.message });
   }
