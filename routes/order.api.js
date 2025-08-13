c09e8da28783b8d55668566848385c434ff7e110
Hyeonjin <oboki2001@naver.com>
2025-08-13 20:16:44 +0900

    [feat] 주문 조회·수정 라우트 추가

--- routes/order.api.js
+++ routes/order.api.js
@@ -4,5 +4,25 @@ const orderController = require("../controllers/order.controller");
 const router = express.Router();
 
 router.post("/", authCountroller.authenticate, orderController.createOrder);
+// 내 주문 목록 조회
+router.get("/", authCountroller.authenticate, orderController.readOrderList);
+// 모든 주문 목록 조회 (관리자)
+router.get(
+  "/admin",
+  authCountroller.authenticate,
+  authCountroller.checkAdminPermission,
+  orderController.readOrderListForAdmin
+);
+router.get(
+  "/:id",
+  authCountroller.authenticate,
+  orderController.readOrderDetail
+);
+router.put(
+  "/:id",
+  authCountroller.authenticate,
+  authCountroller.checkAdminPermission,
+  orderController.updateOrder
+);
 
 module.exports = router;
